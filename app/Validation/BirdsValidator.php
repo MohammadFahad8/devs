840c52733d663449ba2fa11af79cfeb63cdf168e
MohammadFahadShamim <fahad.shamim@devsinc.com>
2022-09-09 19:34:46 +0500

    cat

--- app/Validation/BirdsValidator.php
+++ app/Validation/BirdsValidator.php
@@ -6,10 +6,10 @@ class BirdsValidator
 {
     public static function validate($input)
     {
-        $rules = [
-            'title' => 'Required|Min:4|Max:80|alpha_spaces',
-            'body' => 'Required',
-        ];
-        return Validator::make($input, $rules);
+        // $rules = [
+        //     'title' => 'Required|Min:4|Max:80|alpha_spaces',
+        //     'body' => 'Required',
+        // ];
+        // return Validator::make($input, $rules);
     }
 }
